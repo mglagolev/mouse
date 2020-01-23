309819e1cdc87790183b40e3b1c6f46fd3b45876
Mikhail Glagolev <mikhail.glagolev@gmail.com>
2020-01-23 20:03:29 +0300

    Adding a tool to produce lists of aggregated atoms

---
+++ calculate_aggregation.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import argparse
+from lammpack_types import Config
+from ordering_functions import *
+
+parser = argparse.ArgumentParser(description = 'Determine the aggregates in the systems and print out the atoms belonging to each of them.')
+
+parser.add_argument('frames', metavar = 'ATOMIC COORDINATES', type = str, nargs = '+', help = 'data file')
+
+parser.add_argument('--atomtypes', type = str, nargs = '+', help = 'types of atoms')
+
+parser.add_argument('--threshold', type = float, nargs = '?', default = 1.2, help = 'maximum distance between the atoms to be considered as clustered')
+
+args = parser.parse_args()
+
+for in_data in args.frames:
+	print >> sys.stderr, '\r',
+	frame = read_data_typeselect(in_data)
+	sys.stderr.write('Read frame (natom): '+str(frame.n_atom())+'\n')
+	frame = selectByAtomFields(frame, { "type" : args.atomtypes })
+	sys.stderr.write('Selected by type: '+str(frame.n_atom())+'\n')
+	frame.reassign_inums()
+	sys.stderr.write('Box size: x ' + str(frame.box().x) + ' y ' + str(frame.box().y) + ' z ' + str(frame.box().z) + '\n')
+	makeNeighborlistsFromDistances(frame, threshold = args.threshold)
+	all_aggregates = determineClusters(frame)
+	for aggregate in all_aggregates:
+		atom_nums_array = []
+		for atom in aggregate:
+			atom_nums_array.append(atom.num)
+		atom_nums_array.sort()
+		sys.stdout.write(str(atom_nums_array) + "\n")
+		
