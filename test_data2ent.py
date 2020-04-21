9c5e5e2ec113ab44d80716a0eb67e817a14cbb56
Mikhail Glagolev <mikhail.glagolev@gmail.com>
2020-04-22 00:29:17 +0300

    Adding an option to center the simulation cell around zero

--- test_data2ent.py
+++ test_data2ent.py
@@ -1,10 +1,21 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import sys
 from lammpack_types import Config
+import argparse
+import vector3d
+
+parser = argparse.ArgumentParser(description = 'Convert LAMMPS data file into PDB')
+parser.add_argument('input', action = "store", help = "input file")
+parser.add_argument('output', action = "store", help = "output file")
+parser.add_argument("--center", action = "store_true", help = "Center the box around zero")
+args = parser.parse_args()
 
 frame = Config()
 
-frame.read_lmp_data(sys.argv[1])
+frame.read_lmp_data(args.input)
 
-frame.write_pdb(sys.argv[2])
+if args.center:
+	frame.center_box()
+
+frame.write_pdb(args.output)
