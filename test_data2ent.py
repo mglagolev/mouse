ca1b0a9327ec87d8aa0a3e1faea339bcfb3946eb
Mikhail Glagolev <mikhail.glagolev@gmail.com>
2021-02-10 13:05:04 +0300

    Adding control of PBC bonds visibility as a command line option

--- test_data2ent.py
+++ test_data2ent.py
@@ -4,18 +4,25 @@ import sys
 from lammpack_types import Config
 import argparse
 import vector3d
+import lammpack_misc
 
 parser = argparse.ArgumentParser(description = 'Convert LAMMPS data file into PDB')
 parser.add_argument('input', action = "store", help = "input file")
 parser.add_argument('output', action = "store", help = "output file")
 parser.add_argument("--center", action = "store_true", help = "Center the box around zero")
+parser.add_argument("--hide-pbc-bonds", action = "store_true", help = "Hide the bonds transversing the periodic boundary conditions")
+parser.add_argument('--atomtypes', type = str, nargs = '+', help = 'types of atoms')
+
 args = parser.parse_args()
 
 frame = Config()
 
 frame.read_lmp_data(args.input)
 
+if args.atomtypes != []:
+	frame = lammpack_misc.selectByAtomFields(frame, { "type" : args.atomtypes })
+
 if args.center:
 	frame.center_box()
 
-frame.write_pdb(args.output)
+frame.write_pdb(args.output, args.hide_pbc_bonds)
