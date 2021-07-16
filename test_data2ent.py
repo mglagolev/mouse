afa0b7a3e8495327c85a922391a0a0f6be09d7e2
Mikhail Glagolev <mikhail.glagolev@gmail.com>
2021-07-16 13:19:59 +0300

    Features for single molecule systems: 1. An option to switch to uncut coordinates; 2. An option to shift system COM to zero

--- test_data2ent.py
+++ test_data2ent.py
@@ -9,6 +9,8 @@ import lammpack_misc
 parser = argparse.ArgumentParser(description = 'Convert LAMMPS data file into PDB')
 parser.add_argument('input', action = "store", help = "input file")
 parser.add_argument('output', action = "store", help = "output file")
+parser.add_argument('--uncut', action = "store_true", help = "Convert the coordinates of the atoms to uncut")
+parser.add_argument('--center-com', action = "store_true", help = "Move the center of mass of the system to zero")
 parser.add_argument("--center", action = "store_true", help = "Center the box around zero")
 parser.add_argument("--hide-pbc-bonds", action = "store_true", help = "Hide the bonds transversing the periodic boundary conditions")
 parser.add_argument('--atomtypes', type = str, nargs = '+', help = 'types of atoms')
@@ -21,6 +23,13 @@ frame.read_lmp_data(args.input, angles = False, dihedrals = False)
 
 if args.atomtypes is not None:
 	frame = lammpack_misc.selectByAtomFields(frame, { "type" : args.atomtypes })
+
+if args.uncut:
+	frame.convertCoordsCutToUncut()
+	
+if args.center_com:
+	sys.stderr.write("COM: " + str(frame.com()) + "\n")
+	frame.com_to_zero()
 
 if args.center:
 	frame.center_box()
